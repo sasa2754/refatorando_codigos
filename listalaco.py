e31e71a86500f8af4c33458c608592b94dc8135b
sas2754 <sasamor5@gmail.com>
2024-09-13 15:52:33 -0300

    listalaco

---
+++ listalaco.py
@@ -0,0 +1,8 @@
+idade = int(input('Informe sua idade: '))
+salario = int(input('Informe seu salario: '))
+
+for i in range (11):
+    print(f'{idade} anos e ganha {salario} ({i})')
+
+
+# Tbm n entendi o pq desse código, mas faz parte ksakskaskak
